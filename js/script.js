e13b8a39e87f415924c197f9199d604697e9e317
depthgr8 <depthgr8@gmail.com>
2026-02-19 16:15:14 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Jalte Diye Foundation - Website JavaScript
+// This file contains JavaScript functionality for the website
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+// Add any additional website functionality here
